f78bab9b7772699490618a4b56ee8ad30073ecce
Lucia <hyeyoung9115@gmail.com>
2022-08-23 19:45:19 +0900

    :tada: Project setup
    Project setup

---
+++ src/styled.d.ts
@@ -0,0 +1,9 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    textColor: string;
+    bgClolor: string;
+    accentColor: string;
+  }
+}
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgClolor: "#2f3640",
+  textColor: "black",
+  accentColor: "#9c88ff",
+};
